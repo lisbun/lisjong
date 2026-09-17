f13b1cb391b3f76637927a7596a114045d3882d9
lisbun <8768230+lisbun@users.noreply.github.com>
2026-09-18 07:10:56 +0900

    Export targeted honor-release policy

--- src/lisjong/policies/__init__.py
+++ src/lisjong/policies/__init__.py
@@ -22,6 +22,9 @@ from lisjong.policies.mechanism_riichi_defense_yakuhai_call import (
 from lisjong.policies.minimal import MinimalPolicy
 from lisjong.policies.open_hand_yaku_aware_call import OpenHandYakuAwareCallPolicy
 from lisjong.policies.shanten import ShantenPolicy
+from lisjong.policies.targeted_honor_release_terminal_progression import (
+    TargetedHonorReleaseTerminalProgressionPolicy,
+)
 from lisjong.policies.terminal_shanten_progression_mechanism_riichi_defense import (
     TerminalShantenProgressionMechanismRiichiDefensePolicy,
 )
@@ -43,6 +46,7 @@ __all__ = [
     "MinimalPolicy",
     "OpenHandYakuAwareCallPolicy",
     "ShantenPolicy",
+    "TargetedHonorReleaseTerminalProgressionPolicy",
     "TerminalShantenProgressionMechanismRiichiDefensePolicy",
     "TwoStepUkeirePolicy",
     "UkeirePolicy",
